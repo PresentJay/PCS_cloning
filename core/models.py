f050fad69802e29a84e2d64095e9d6171be5f00b
PresentJay <presentj94@gmail.com>
2020-05-17 10:50:54 +0900

    core modeling : core-app is base of other apps (abstract-model) : it will don't coded admin, views, apps, e.t.c

--- core/models.py
+++ core/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+
+class TimeStampedModel(models.Model):
+
+    """ created time / updated time stamping model """
+
+    created = models.DateTimeField(auto_now_add=True)
+    # option "auto_now_add" : record just first timeline as created
+    updated = models.DateTimeField(auto_now=True)
+    # option "auto_now" : record every timeline as modified
+
+    class Meta:
+        abstract = True
+        # abstract Model setting in python
